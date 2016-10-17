75cf26c3d73b9d11901359026fb02986ef26351a
Derick Baker <dbaker@hartenergy.com>
2016-10-17 11:22:33 -0500

    Added Agenda page

--- scripts/lib/zPageLoad.js
+++ scripts/lib/zPageLoad.js
@@ -4,8 +4,8 @@ $(window).load(function() { // makes sure the whole site is loaded
 
     document.body.className += ' loaded'
     
-    $('#preloader').delay(400).fadeOut('slow'); // will fade out the white DIV that covers the website.
-    $('#preloader .inner').fadeOut(); // will first fade out the loading animation
+    //$('#preloader').delay(400).fadeOut('slow'); // will fade out the white DIV that covers the website.
+    //$('#preloader .inner').fadeOut(); // will first fade out the loading animation
 
     //var wow = new WOW({
         //offset:100,        // distance to the element when triggering the animation (default is 0)
@@ -25,6 +25,30 @@ $(window).load(function() { // makes sure the whole site is loaded
 })
 
 $(document).ready(function() {
+    
+    $(".animsition").animsition({
+        inClass: 'fade-in',
+        outClass: 'fade-out',
+        inDuration: 1500,
+        outDuration: 800,
+        linkElement: '.animsition-link',
+        // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+        loading: true,
+        loadingParentElement: 'body', //animsition wrapper element
+        loadingClass: 'animsition-loading',
+        loadingInner: '', // e.g '<img src="loading.svg" />'
+        timeout: false,
+        timeoutCountdown: 5000,
+        onLoadEvent: true,
+        browser: [ 'animation-duration', '-webkit-animation-duration'],
+        // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+        // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+        overlay : false,
+        overlayClass : 'animsition-overlay-slide',
+        overlayParentElement : 'body',
+        transition: function(url){ window.location.href = url; }
+  });
+
     $('.menu-link').bigSlide({
         menu: '#fullMenu',
         side: 'right',
